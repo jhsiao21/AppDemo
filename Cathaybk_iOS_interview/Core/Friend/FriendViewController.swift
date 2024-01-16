251ec6118e3d046c36ba90ca2b0f313484353a6a
Logan <logan.hsiao.jk@gmail.com>
2024-01-16 11:25:20 +0800

    upload

--- Cathaybk_iOS_interview/Core/Friend/FriendViewController.swift
+++ Cathaybk_iOS_interview/Core/Friend/FriendViewController.swift
@@ -12,7 +12,6 @@ import RxCocoa
 class FriendViewController: UIViewController {
     var scenario : Scenario
     var viewModel = FriendTableViewModel()
-    let vStackView = UIStackView()
     let profileView =  ProfileView()
     var inviteContainer = InviteContainer()
     let selectFilterView = SelectedFilterView()
@@ -27,6 +26,8 @@ class FriendViewController: UIViewController {
     var tableViewTopConstraint2: NSLayoutConstraint?
     var selectFilterViewTopConstraint1: NSLayoutConstraint?
     var selectFilterViewTopConstraint2: NSLayoutConstraint?
+    var tbHeaderViewTopConstraint1: NSLayoutConstraint?
+    var tbHeaderViewTopConstraint2: NSLayoutConstraint?
 
     private let tableView: UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
@@ -65,18 +66,18 @@ class FriendViewController: UIViewController {
     // MARK: - Bind
     private func bind() {
         
-        // 搜索结果数据
+        // 搜尋結果資料
         let searchResults = searchController.searchBar.rx.text.orEmpty
-            .debounce(RxTimeInterval.milliseconds(300), scheduler: MainScheduler.instance)
-            .distinctUntilChanged()
-            .flatMapLatest { query in
+            .debounce(RxTimeInterval.milliseconds(300), scheduler: MainScheduler.instance) //debounce用於限制請求的頻率，避免因用戶輸入的每一個字符變化而觸發過多的搜索操作
+            .distinctUntilChanged() //distinctUntilChanged確保僅在搜索詞發生變化時才觸發搜索
+            .flatMapLatest { query in //flatMapLatest用於根據當前的查詢字符串決定是提供完整好友列表還是根據搜索詞過濾的列表
                 query.isEmpty
                 ? self.viewModel.friendElements.asObservable()
                 : self.viewModel.searchFriends(query: query)
             }
-            .share(replay: 1)
+            .share(replay: 1) //確保多個訂閱者可以接收到最近一次的搜索結果
         
-        // 根据搜索栏的状态决定展示数据
+        //結合搜尋框輸入和搜尋結果更新表格視圖
         Observable.combineLatest(viewModel.friendElements, searchResults, searchController.searchBar.rx.text.orEmpty) { friendElements, searchResults, query in
             return query.isEmpty ? friendElements : searchResults
         }
@@ -84,27 +85,33 @@ class FriendViewController: UIViewController {
             cell.friendElement = item
         }
         .disposed(by: disposeBag)
-        
-        
-        // 配置搜索控制器的状态变化
+                
+        //監聽搜尋框文字變化
         searchController.searchBar.rx.text.orEmpty
             .bind(onNext: { [weak self] _ in
+                //當搜尋框的文字發生變化時，重新加載表格視圖
                 self?.tableView.reloadData()
             })
             .disposed(by: disposeBag)
         
+        //監聽好友列表資料
         viewModel.friendElements
-            .asDriver() // 将 Observable 转换为 Driver 单元，它会确保在主线程上执行，并且正确地处理错误。
+            .asDriver() // 將Observable轉換為Driver單元，它會確保在主執行緒上執行
             .drive(onNext: { [weak self] friendElements in
+                //friendElements發生變化時，根據是否有好友數據來顯示或隱藏搜索框和其他 UI元件
                 let isEmpty = friendElements.isEmpty
+                self?.searchController.searchBar.isHidden = isEmpty
+                self?.tableView.refreshControl = nil
                 self?.tableView.backgroundView?.isHidden = !isEmpty
                 if !isEmpty {
+                    //有好友資料時的處理
                     self?.inviteContainer.reloadData()
+                    self?.setupRefreshControl()
                 }
             })
             .disposed(by: disposeBag)
         
-        // 监听搜索栏的激活状态
+        // 監聽搜尋控制器的呈現事件
         searchController.rx.didPresent
             .subscribe(onNext: { [unowned self] _ in
                 print("searchController.rx.didPresent")
@@ -113,43 +120,56 @@ class FriendViewController: UIViewController {
             })
             .disposed(by: disposeBag)
         
-        // 监听搜索栏的取消按钮点击事件
+        //監聽搜尋控制器的取消事件
         searchController.searchBar.rx.cancelButtonClicked
             .subscribe(onNext: { [unowned self] _ in
-                // 处理搜索取消的逻辑
                 print("searchController.searchBar.rx.cancelButtonClicked")
-                
                 self.viewVisible(visible: false)
             })
             .disposed(by: disposeBag)
         
+        //監聽用戶資料
         viewModel.userElement
             .asObservable()
             .subscribe(onNext: { [weak profileView = self.profileView] element in
+                //當資料更新時配置用戶界面
                 profileView?.configure(userData: element.first)
             })
             .disposed(by: disposeBag)
     }
     
     override func viewDidLayoutSubviews() {
-#if DEBUG
         print("viewDidLayoutSubviews")
-#endif
+        
+        inviteShowUpEvent()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        
+    }
+    
+    private func inviteShowUpEvent() {
+        
+        if inviteContainer.numberOfVisibleCards > 0 {
+            //如果邀請列有邀請，selectFilterView就往下移
+            selectFilterViewTopConstraint1?.isActive = true
+            selectFilterViewTopConstraint2?.isActive = false
+        } else {
+            //如果邀請列沒有邀請，selectFilterView就往上移
+            selectFilterViewTopConstraint1?.isActive = false
+            selectFilterViewTopConstraint2?.isActive = true
+        }
     }
     
     func style() {
         
         view.backgroundColor = .white
         
-        vStackView.axis = .vertical
-        vStackView.backgroundColor = UIColor.backgroundWhite
-        vStackView.spacing = 35
-        vStackView.translatesAutoresizingMaskIntoConstraints = false
-        
         profileView.translatesAutoresizingMaskIntoConstraints = false
         
-        inviteContainer.translatesAutoresizingMaskIntoConstraints = false
-        inviteContainer.delegate = self
+//        inviteContainer.translatesAutoresizingMaskIntoConstraints = false
+//        inviteContainer.delegate = self
         
         selectFilterView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -184,6 +204,10 @@ class FriendViewController: UIViewController {
         tableView.backgroundView?.isHidden = true
         
         //Setup refresh
+        setupRefreshControl()
+    }
+    
+    private func setupRefreshControl() {
         refreshControl = UIRefreshControl()
         refreshControl.attributedTitle = NSAttributedString(string: "更新中..")
         tableView.refreshControl = refreshControl
@@ -194,19 +218,25 @@ class FriendViewController: UIViewController {
         
         configureNavbar()
         view.addSubview(profileView)
-        view.addSubview(inviteContainer)
+//        view.addSubview(inviteContainer)
         view.addSubview(selectFilterView)
-        view.addSubview(separatorView!)
+//        view.addSubview(separatorView!)
         view.addSubview(tableView)
 //        tableView.addSubview(addFriendButton)
         tableView.tableHeaderView = searchController.searchBar
         
-        inviteContainerHeightConstraint = inviteContainer.heightAnchor.constraint(equalToConstant: 70)
+//        inviteContainerHeightConstraint = inviteContainer.heightAnchor.constraint(equalToConstant: 70)
         
         tableViewTopConstraint1 = tableView.topAnchor.constraint(equalTo: selectFilterView.bottomAnchor, constant: 0.5)
         tableViewTopConstraint1?.isActive = true
         
         tableViewTopConstraint2 = tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        
+        selectFilterViewTopConstraint1 = selectFilterView.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 32)
+        
+//        selectFilterViewTopConstraint2 = selectFilterView.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 30)
+        
+//        tbHeaderViewTopConstraint1 = tableView.tableHeaderView!.topAnchor.constraint(equalTo: tableView.topAnchor)
                 
         NSLayoutConstraint.activate([
             
@@ -214,24 +244,23 @@ class FriendViewController: UIViewController {
             profileView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: profileView.trailingAnchor, multiplier: 3),
             
-            inviteContainer.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
-            inviteContainer.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 35),
-            view.trailingAnchor.constraint(equalToSystemSpacingAfter: inviteContainer.trailingAnchor, multiplier: 3),
+//            inviteContainer.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
+//            inviteContainer.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 35),
+//            view.trailingAnchor.constraint(equalToSystemSpacingAfter: inviteContainer.trailingAnchor, multiplier: 3),
             
-            inviteContainerHeightConstraint!,
+//            inviteContainerHeightConstraint!,
             
-            selectFilterView.topAnchor.constraint(equalTo: inviteContainer.bottomAnchor, constant: 32),
+            selectFilterView.topAnchor.constraint(equalTo: profileView.bottomAnchor, constant: 32),
             selectFilterView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 3),
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: selectFilterView.trailingAnchor, multiplier: 3),
             
-            separatorView!.topAnchor.constraint(equalTo: selectFilterView.bottomAnchor),
-            separatorView!.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            separatorView!.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            separatorView!.heightAnchor.constraint(equalToConstant: 0.5),
-            separatorView!.widthAnchor.constraint(equalToConstant: view.bounds.width),
+//            separatorView!.topAnchor.constraint(equalTo: selectFilterView.bottomAnchor),
+//            separatorView!.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            separatorView!.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//            separatorView!.heightAnchor.constraint(equalToConstant: 0.5),
+//            separatorView!.widthAnchor.constraint(equalToConstant: view.bounds.width),
             
             tableViewTopConstraint1!,
-                        
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
@@ -241,7 +270,7 @@ class FriendViewController: UIViewController {
             view.trailingAnchor.constraint(equalToSystemSpacingAfter: tableView.backgroundView!.trailingAnchor, multiplier: 3),
             tableView.backgroundView!.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             
-//            tableView.tableHeaderView!.topAnchor.constraint(equalTo: tableView.topAnchor),
+//            tbHeaderViewTopConstraint1!,
 //            tableView.tableHeaderView!.leadingAnchor.constraint(equalTo: tableView.leadingAnchor),
 //            tableView.tableHeaderView!.trailingAnchor.constraint(equalTo: tableView.trailingAnchor),
 //            
@@ -307,9 +336,9 @@ class FriendViewController: UIViewController {
     @objc func refreshHandler() {
         print("refreshHandler")
         //延遲0.5秒
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) { [self] in
-            viewModel.fetchFriendData(scenario: self.scenario)
-            self.tableView.refreshControl?.endRefreshing()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.5) { [weak self] in
+            self?.viewModel.fetchFriendData(scenario: Scenario(rawValue: (self?.scenario)!.rawValue) ?? .無好友畫⾯)
+            self?.tableView.refreshControl?.endRefreshing()
         }
     }
     
@@ -379,17 +408,14 @@ extension FriendViewController : UITableViewDelegate, UITableViewDataSource {
 //MARK: - 邀請卡片容器
 extension FriendViewController: CardContainerDataSource, CardContainerDelegate {
     
+    //inviteContainer.reloadData()時驅動numberOfCardsToShow
     func numberOfCardsToShow() -> Int {
                 
         let num = viewModel.friendElements.value.filter {
             $0.status == .inviting
         }.count
         
-        if num > 0 {
-            inviteContainer.isHidden = false
-        } else {
-            inviteContainer.isHidden = true
-        }
+        inviteShowUpEvent()
         
         return num
     }
@@ -415,10 +441,8 @@ extension FriendViewController: CardContainerDataSource, CardContainerDelegate {
     //回覆邀請的處理函式
     func card(_ view: InviteCardView, at index: Int) {
         
-        //如果邀請列沒有邀請就隱藏
-        if inviteContainer.numberOfVisibleCards < 1 {
-            inviteContainer.isHidden = true
-        }
+        
+//        inviteShowUpEvent() //在viewDidLayoutSubviews()內比較好，因為一開始沒有資料
         
         if let indexInFriendElements = viewModel.friendElements.value
             .firstIndex(where: { $0.name == view.nameLabel.text }) {
